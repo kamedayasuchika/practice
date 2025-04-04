43f4338078b2c370e4061f6cfd09a31408be701f
kamedayasuchika <kame.1230yk.chika@gmail.com>
2025-04-04 14:51:26 +0900

    作成

---
+++ practice/src/practice_3/Practice_3_01.java
@@ -0,0 +1,22 @@
+package practice_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practice_3_01 {
+	
+	public static void main(String[] args) throws IOException{
+
+	BufferedReader br = 
+		new BufferedReader(new InputStreamReader( System.in ) );
+
+		int x = Integer.parseInt( br.readLine() );
+		int y = Integer.parseInt( br.readLine() );
+
+		if(x > y){
+			System.out.println("xはyより大きい。");
+		}
+	}
+
+}
---
+++ practice/src/practice_3/Practice_3_02.java
@@ -0,0 +1,10 @@
+package practice_3;
+
+public class Practice_3_02 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
