4daa23c15182e8ec7eb329bdb227b1c76e0ca0a7
kamedayasuchika <kame.1230yk.chika@gmail.com>
2025-04-14 14:43:48 +0900

    作成

--- practice/src/practice_4/Practice_4_01.java
+++ practice/src/practice_4/Practice_4_01.java
@@ -2,7 +2,7 @@ package practice_4;
 
 public class Practice_4_01 {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		for(int i = 0; i < 10; i++) {
 			System.out.println("SPAM");
--- practice/src/practice_4/Practice_4_02.java
+++ practice/src/practice_4/Practice_4_02.java
@@ -2,7 +2,7 @@ package practice_4;
 
 public class Practice_4_02 {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		for(int i = 1; i < 10; i++) {
 			System.out.println(3 * i);
 		}
