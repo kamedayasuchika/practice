d8fc88d4e0ce48e965c04501da6454203898502e
kamedayasuchika <kame.1230yk.chika@gmail.com>
2025-04-14 14:44:46 +0900

    あ

--- practice/src/practice_4/Practice_4_04.java
+++ practice/src/practice_4/Practice_4_04.java
@@ -2,7 +2,7 @@ package practice_4;
 
 public class Practice_4_04 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		int x = 1;
 		for(int i = 8; i > 2; i--){
 			x = x * (i - 1);
