16c18d67e1f7192fb5488c189235f2adb31a2399
kamedayasuchika <kame.1230yk.chika@gmail.com>
2025-04-15 15:51:56 +0900

    作成

--- practice/src/practice_4/Practice_4_09.java
+++ practice/src/practice_4/Practice_4_09.java
@@ -1,10 +1,30 @@
 package practice_4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Practice_4_09 {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = 
+			new BufferedReader(new InputStreamReader( System.in ) );
 
+			int max_value = Integer.parseInt( br.readLine() );
+			int min_value = max_value;
+
+			for( int i = 1 ; i < 10 ; i++ ){
+		    int value = Integer.parseInt( br.readLine() );
+
+		    if( value > max_value )
+		        max_value = value;
+
+		    if( value < min_value )
+		        min_value = value;
+		}
+
+		System.out.println( "最大値" + max_value);
+		System.out.println( "最小値" + min_value);
 	}
-
 }
+
---
+++ practice/src/practice_4/Practice_4_10.java
@@ -0,0 +1,19 @@
+package practice_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practice_4_10 {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = 
+			new BufferedReader(new InputStreamReader( System.in ) );
+
+		int x = Integer.parseInt( br.readLine() );
+		
+		for(int i = 0; i < x ; x--) {
+			System.out.println("*");
+		}
+	}
+}
